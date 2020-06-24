a05f7b9e99dac07e18e598c44925a597eecca961
andheroe <freefunning@gmail.com>
2020-06-24 17:33:25 +0300

    rds working

--- rds.tf
+++ rds.tf
@@ -1,14 +1,26 @@
+resource "aws_db_parameter_group" "web_prod_rds_parameter_group" {
+  name   = "web-prod-rds-parameter-group"
+  family = "postgres10"
+
+  parameter {
+    name  = "rds.force_ssl"
+    value = 1  # Force all connections to the database to use SSL (you can get the certificate here https://s3.amazonaws.com/rds-downloads/rds-ca-2019-root.pem)
+  }
+}
+
 resource "aws_db_instance" "web_prod" {
-  identifier           = "web-prod-rds"
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "10.9"
-  instance_class       = "db.t2.micro"
-  name                 = "seamless"
-  username             = "root"
-  password             = data.aws_kms_secrets.web_prod_rds.plaintext["password"]
-  skip_final_snapshot  = true
-  apply_immediately    = true
-  publicly_accessible  = true
+  identifier             = "web-prod-rds"
+  allocated_storage      = 20
+  storage_type           = "gp2"
+  engine                 = "postgres"
+  engine_version         = "10.9"
+  instance_class         = "db.t2.micro"
+  name                   = "seamless"
+  username               = "root"
+  password               = data.aws_kms_secrets.web_prod_rds.plaintext["password"]
+  skip_final_snapshot    = true
+  apply_immediately      = true
+  publicly_accessible    = true
+  vpc_security_group_ids = [aws_security_group.allow_internet_access_to_rds.id]
+  parameter_group_name   = aws_db_parameter_group.web_prod_rds_parameter_group.name
 }
---
+++ security_groups.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "allow_internet_access_to_rds" {
+  name        = "allow_internet_access_to_rds"
+
+  ingress {
+    from_port   = "5432"
+    to_port     = "5432"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}

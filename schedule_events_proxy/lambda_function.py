0f2bcc06b28b792d7f474f179a88a20f97e6eedc
Vadim Yanko <yanko.vadim@gmail.com>
2020-07-10 17:01:24 +0000

    Update endpoint in lambda

--- schedule_events_proxy/lambda_function.py
+++ schedule_events_proxy/lambda_function.py
@@ -5,6 +5,6 @@ import requests
 
 def lambda_handler(event, context):
     print(event)
-    url = f"https://schedule:{os.getenv('SCHEDULE_PASSWORD')}@app.seamlesscloud.io/schedule/jobs/execute"
+    url = f"https://schedule:{os.getenv('SCHEDULE_PASSWORD')}@app.seamlesscloud.io/jobs/execute"
     res = requests.post(url, json=event)
     res.raise_for_status()

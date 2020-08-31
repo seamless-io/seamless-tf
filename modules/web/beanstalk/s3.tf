b3e133976c859aec710a502b7b84f6dfab46da61
andheroe <freefunning@gmail.com>
2020-08-31 23:00:26 +0300

    added s3 bucket for templates

--- modules/web/beanstalk/s3.tf
+++ modules/web/beanstalk/s3.tf
@@ -21,3 +21,17 @@ resource "aws_s3_bucket" "web-jobs" {
     enabled = var.jobs_s3_versioning
   }
 }
+
+resource "aws_s3_bucket" "web-job-templates" {
+  bucket = format("web-%s-job-templates", var.stage)
+  acl    = "private"
+
+  tags = {
+    Name        = format("Web %s Job Templates", var.stage)
+    Environment = var.stage
+  }
+
+  versioning {
+    enabled = var.jobs_s3_versioning
+  }
+}
